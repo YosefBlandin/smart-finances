870e8299f162b368afbc349d809aa701703a2ff2
Yosef Blandin <yosefleanb@gmail.com>
2023-08-29 13:18:37 -0400

    Finished first design for smart finances home

--- src/app/modules/home/containers/home/home.component.ts
+++ src/app/modules/home/containers/home/home.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	public technologies: {
+		name: string
+		src: string
+	}[] = []
 
 	constructor() { }
 
 	ngOnInit(): void {
+		this.initImgs()
+	}
+
+	private initImgs(): void {
+		this.technologies = [
+			{
+				name: 'Angular',
+				src: "../../../../../assets/svg/angular_logo.png"
+			},
+			{
+				name: 'RxJS',
+				src: "../../../../../assets/svg/rxjs.svg"
+			},
+			{
+				name: 'Nest',
+				src: "../../../../../assets/svg/nestjs.svg"
+			},
+		]
 	}
 
 }
