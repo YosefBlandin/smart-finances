c1b34e79bf2b534eac38982e57961b2cdaf140b2
Yosef Blandin <yosefleanb@gmail.com>
2023-08-30 21:38:32 -0400

    Created dashboard module

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
