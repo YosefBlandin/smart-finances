51de9dab8881bdb88f79019c7a3ef7205b2a0f2e
Yosef Blandin <yosefleanb@gmail.com>
2024-05-03 18:59:08 -0400

    Improved mockup of budget

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -11,18 +11,53 @@ import { MatButton } from '@angular/material/button';
 export class DashboardComponent {
     public budgets = [
         {
+            id: 1,
             name: 'Emigration Plan',
             created_at: '2024/05/03',
             start_date: '2024/01/01',
             end_date: '2024/09/15',
             total_savings_goal: 5000,
-            recurring_income: 1000,
-            recurring_expenses: 5000,
-            additional_income: [],
-            additional_expenses: [],
+            recurring_incomes: [
+                {
+                    id: 1,
+                    name: 'Vest Selling',
+                    amount: 100,
+                    frequency: 1, // 1 = daily, 2 = weekly, 3 = monthly, 4 = annual
+                    created_at: '2024/07/03',
+                    start_date: '2024/07/03',
+                    end_date: '',
+                },
+            ],
+            recurring_expenses: [
+                {
+                    id: 1,
+                    name: 'Internet Services',
+                    amount: 40,
+                    frequency: 3, // 1 = daily, 2 = weekly, 3 = monthly, 4 = annual
+                    created_at: '2024/07/03',
+                    start_date: '2024/07/03',
+                    end_date: '',
+                },
+            ],
+            additional_income: [
+                {
+                    id: 1,
+                    name: 'Vest Selling',
+                    amount: 100,
+                    created_at: '2024/07/03',
+                },
+            ],
+            additional_expenses: [
+                {
+                    id: 1,
+                    name: 'Groceries',
+                    amount: 300,
+                    created_at: '2024/01/02',
+                },
+            ],
             total_income: 0,
             total_expenses: 0,
-            net_income: 0,
+            net_income_saved: 0,
             amount_needed_to_save: 0,
         },
     ];
