e3e70bd4074eee794b38c5d43a1a848bf1a54edb
Yosef Blandin <yblandin@emsys-solutions.net>
2024-05-16 12:16:27 -0400

    Moved retry intercepto to core/interceptors

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -2,7 +2,6 @@ import { errorsInterceptor } from './errors/errors.interceptor';
 import { retryInterceptor } from './retry/retry.interceptor';
 import { sessionInterceptor } from './session/session.interceptor';
 
-/** Array of Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
     retryInterceptor,
     errorsInterceptor,
