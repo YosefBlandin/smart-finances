4051643dd46b7dcf663ced5af1eee9ba92b480d7
Yosef Blandin <yosefleanb@gmail.com>
2024-05-25 08:48:59 -0400

    Adjusting data table styles

--- src/app/budgets/pages/budgets/budgets.component.ts
+++ src/app/budgets/pages/budgets/budgets.component.ts
@@ -1,35 +1,18 @@
-import {
-  AfterViewInit,
-  Component,
-  ViewChild,
-  signal,
-} from '@angular/core';
+import { AfterViewInit, Component, ViewChild, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import {
-  DataTableActionsComponent,
-  DataTableComponent,
-  InputComponent,
-} from '../../../shared';
+import { DataTableComponent, InputComponent } from '../../../shared';
 import { MatButton } from '@angular/material/button';
 import { MatDrawer } from '@angular/material/sidenav';
 @Component({
   selector: 'app-budgets',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    InputComponent,
-    MatButton,
-    DataTableComponent,
-    DataTableActionsComponent,
-  ],
+  imports: [ReactiveFormsModule, InputComponent, MatButton, DataTableComponent],
   templateUrl: './budgets.component.html',
   styleUrl: './budgets.component.scss',
 })
 export class BudgetsComponent implements AfterViewInit {
   @ViewChild('drawer') matDrawer!: MatDrawer;
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+  constructor(private formBuilder: FormBuilder) {}
   public expenseForm = this.formBuilder.group({
     expense_name: this.formBuilder.control('', []),
     expense_amount: this.formBuilder.control('', []),
