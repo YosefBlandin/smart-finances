70d70db38cdbcdc92b30adafba420980f188a8a8
Yosef Blandin <yosefleanb@gmail.com>
2024-06-04 19:55:54 -0400

    Fixed conflicts in data-table-actions

--- src/app/shared/components/data-table-actions/data-table-actions.component.ts
+++ src/app/shared/components/data-table-actions/data-table-actions.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
-<<<<<<< HEAD
   selector: 'app-data-table-actions-component',
   standalone: true,
   imports: [MatIcon],
@@ -20,18 +19,6 @@ export class DataTableActionsComponent {
     actionName: string;
     elementIndex: number;
   }>();
-=======
-  selector: 'smart-finances-data-table-actions-component',
-  standalone: true,
-  imports: [MatIcon],
-  templateUrl: './data-table-actions.component.html',
-  styleUrl: './data-table-actions.component.css'
-})
-export class DataTableActionsComponent {
-  @Input() public actions: { icon: string, actionName: string, disabled: boolean }[] = [];
-  @Input() public elementIndex!: number;
-  @Output() public _handleAction = new EventEmitter<{ actionName: string, elementIndex: number }>();
->>>>>>> main
 
   public handleAction(actionName: string, elementIndex: number): void {
     this._handleAction.emit({ actionName, elementIndex });
