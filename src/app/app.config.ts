ff99de3b79ae00162737715cd53867bc1afbb1d5
Yosef Blandin <yosefleanb@gmail.com>
2024-06-11 00:28:53 -0400

    Fixed unwanted api key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,6 +17,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
-    DatePipe, provideFirebaseApp(() => initializeApp({"projectId":"smart-finances-c4764","appId":"1:717485148267:web:26f1d59c4dccba0829892e","storageBucket":"smart-finances-c4764.appspot.com","apiKey":"AIzaSyCgQMnMNUs0LgVkIyIpqrhP2I_NBpeD9GQ","authDomain":"smart-finances-c4764.firebaseapp.com","messagingSenderId":"717485148267","measurementId":"G-MTQR5WDTFV"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+    DatePipe, provideFirebaseApp(() => initializeApp({"projectId":"smart-finances-c4764","appId":"1:717485148267:web:26f1d59c4dccba0829892e","storageBucket":"smart-finances-c4764.appspot.com","apiKey":"API_KEY","authDomain":"smart-finances-c4764.firebaseapp.com","messagingSenderId":"717485148267","measurementId":"G-MTQR5WDTFV"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
